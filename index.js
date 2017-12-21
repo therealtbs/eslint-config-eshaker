fdf4f1c58a1ff660b9d665e7de4b84a42de57d44
Tobias Westfeld <tobias.westfeld@eshaker.de>
2017-12-21 16:14:24 +0100

    [TASK] First version

---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base/rules/best-practices',
+    'eslint-config-airbnb-base/rules/errors',
+    'eslint-config-airbnb-base/rules/node',
+    'eslint-config-airbnb-base/rules/style',
+    'eslint-config-airbnb-base/rules/variables',
+    'eslint-config-airbnb-base/rules/es6',
+    'eslint-config-airbnb-base/rules/strict',
+    'eslint-config-airbnb/rules/react',
+    'eslint-config-airbnb/rules/react-a11y',
+    './base',
+  ].map(require.resolve),
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  plugins: ['redux-saga', 'react', 'jsx-a11y'],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'jsx-a11y/aria-props': 2,
+    'jsx-a11y/heading-has-content': 0,
+    'jsx-a11y/label-has-for': 2,
+    'jsx-a11y/mouse-events-have-key-events': 2,
+    'jsx-a11y/role-has-required-aria-props': 2,
+    'jsx-a11y/role-supports-aria-props': 2,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        aspects: ['preferButton'],
+      },
+    ],
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-target-blank': 0,
+    'react/require-extension': 0,
+    'react/self-closing-comp': 0,
+    'react/no-typos': 0, // https://github.com/yannickcr/eslint-plugin-react/issues/1607
+    'redux-saga/no-yield-in-race': 2,
+    'redux-saga/yield-effects': 2,
+  },
+  overrides: [
+    {
+      files: ['Containers/**/*.js'],
+
+      // Override config (only applied to files that match the given globs)
+      rules: {
+        'react/prefer-stateless-function': 0,
+      },
+    },
+  ],
+};
